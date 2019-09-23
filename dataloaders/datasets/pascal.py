a70ce7a9f6421d192d2433327411bb2edd0a7642
jaemin93 <icebanana93@gmail.com>
2019-09-23 11:45:43 +0900

    tune my dataset

--- dataloaders/datasets/pascal.py
+++ dataloaders/datasets/pascal.py
@@ -11,7 +11,7 @@ class VOCSegmentation(Dataset):
     """
     PascalVoc dataset
     """
-    NUM_CLASSES = 21
+    NUM_CLASSES = 41
 
     def __init__(self,
                  args,
@@ -47,7 +47,7 @@ class VOCSegmentation(Dataset):
                 lines = f.read().splitlines()
 
             for ii, line in enumerate(lines):
-                _image = os.path.join(self._image_dir, line + ".jpg")
+                _image = os.path.join(self._image_dir, line + ".png")
                 _cat = os.path.join(self._cat_dir, line + ".png")
                 assert os.path.isfile(_image)
                 assert os.path.isfile(_cat)
